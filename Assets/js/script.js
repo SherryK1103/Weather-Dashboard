89dd0f1f390514011178ca1615442dc82611f042
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-07-16 10:12:27 -0500

    added variables and API call to JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,21 @@
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', function() {
+    const searchInput = document.getElementById('searchInput');
+    const city = searchInput.value;
+
+    const apiKey = 'fb76df9a3f66718bed25c952ed121f1b';
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={fb76df9a3f66718bed25c952ed121f1b}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+        })
+        .catch(error => {
+        console.error(error);
+    });
+});
 
 
