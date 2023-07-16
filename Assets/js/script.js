b41639dc099d6668631ada27367bc285ff5c843c
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-07-16 11:40:31 -0500

    commented out previously estab function and added new functions for Geo and map API calls

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-const searchButton = document.getElementById('searchButton');
+/* const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', function() {
     const searchInput = document.getElementById('searchInput');
@@ -16,6 +16,42 @@ searchButton.addEventListener('click', function() {
         .catch(error => {
         console.error(error);
     });
-});
+}); */
 
-
+function handleSearch() {
+    const searchInput = document.getElementById('searchInput');
+    const city = searchInput.value;
+  
+    const apiMapKey = 'fb76df9a3f66718bed25c952ed121f1b';
+  
+    const mapUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${fb76df9a3f66718bed25c952ed121f1b}`;
+  
+    fetch(mapUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', handleSearch);
+  
+  function getGeocodingData(city) {
+    const apiGeoKey = '5b17d158e4ae1c29c8841402bf27bc4d';
+    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${5b17d158e4ae1c29c8841402bf27bc4d}`;
+  
+    return fetch(goeUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          const latitude = data[0].lat;
+          const longitude = data[0].lon;
+          return { latitude, longitude };
+        } else {
+          throw new Error('No coordinates found for the specified city.');
+        }
+      });
+  }
