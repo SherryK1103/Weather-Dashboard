42eab603236ae3937c21057993644ce004333994
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-07-16 13:13:44 -0500

    repasting code in js and trying again

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,34 @@
-
-
-
-
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={5b17d158e4ae1c29c8841402bf27bc4d}
-
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={fb76df9a3f66718bed25c952ed121f1b}
-
+function handleSearch() {
+    const searchInput = document.getElementById('searchInput');
+    const city = searchInput.value;
+   
+    const mapUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${fb76df9a3f66718bed25c952ed121f1b}`;
+  
+    fetch(mapUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', handleSearch);
+  
+  function getGeocodingData(city) {
+    const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={5b17d158e4ae1c29c8841402bf27bc4d}`;
+  
+    return fetch(geoUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          const latitude = data[0].lat;
+          const longitude = data[0].lon;
+          return { latitude, longitude };
+        } else {
+          throw new Error('No coordinates found for the specified city.');
+        }
+      });
+  }
